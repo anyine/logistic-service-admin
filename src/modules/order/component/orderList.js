5828e5c3cad78ad5fb1d737dcb8a0028c790a17f
钟正 <616028858@qq.com>
2018-05-10 17:14:46 +0800

    修改官网

--- src/modules/order/component/orderList.js
+++ src/modules/order/component/orderList.js
@@ -18,17 +18,70 @@ const getCompanyListUrl = restUrl.ADDR + 'company/GetCompanyList';
 const updateComCultureUrl = restUrl.ADDR + 'company/updateComCulture';
 //更新公司相册
 const updateComPhotoUrl = restUrl.ADDR + 'company/updateComPhoto';
+//获取公司服务信息
+const getServiceListUrl = restUrl.ADDR + 'company/GetServiceList';
 
 
 class OrderList extends React.Component {
     constructor(props) {
         super(props);
 
+        this.columns = [{
+            title: '名称',
+            dataIndex: 'dish_title',
+            key: 'name',
+            render: (text, record, index) => (
+                <Link to={this.detailrouter(record.id)}>{text}</Link>
+            )
+        }, {
+            title: '创建时间',
+            dataIndex: 'create_time',
+            key: 'create_time',
+        }, {
+            title: <a><Icon type="setting" style={{fontSize: 18}} /></a>,
+            key: 'operation',
+            fixed: 'right',
+            width: 100,
+            render: (text, record, index) => (
+                <Dropdown 
+                    overlay={
+                        <Menu>
+                            <Menu.Item>
+                              <Link to={this.detailrouter(record.id)}>详情</Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                              <Link to={this.editrouter(record.id)}>编辑</Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Popconfirm 
+                                    title="确定要删除吗?" 
+                                    cancelText="取消"
+                                    okText="确定"
+                                    placement="leftTop"
+                                    onConfirm={() => this.onDelete(record.key)}
+                                >
+                                    <a href="#">删除</a>
+                                </Popconfirm>
+                            </Menu.Item>
+                        </Menu>
+                    }
+                >
+                    <a className="ant-dropdown-link">操作</a>
+                </Dropdown>
+            ),
+        }];
+
         this.state = {
             data_1: {},
+            data_2: {},
             editorState_1: EditorState.createEmpty(),
             editorState_2: EditorState.createEmpty(),
-            fileList: [],
+            service_1: [],
+            service_2: [],
+            holiday_1: [],
+            holiday_2: [],
+            fileList_1: [],
+            fileList_2: [],
             loading: false
         };
     }
@@ -46,6 +99,21 @@ class OrderList extends React.Component {
                 let backData = data.backData;
                 backData.map(item => {
                     let editorState = EditorState.createEmpty();
+                    let photos = item.photo.split(',');
+                    let photoList = [];
+                    photos.map((photo, index) => {
+                        photoList.push({
+                            uid: photo,
+                            name: photo + '.png',
+                            status: 'done',
+                            url: restUrl.BASE_HOST + 'UpLoadFile/' + photo + '.png',
+                            response: {
+                                data: {
+                                    id: photo
+                                }
+                            }
+                        });
+                    });
                     if(item.culture && item.culture !== '') {
                         item.culture = draftToHtml(JSON.parse(item.culture));
                         const contentBlock = htmlToDraft(item.culture);
@@ -55,9 +123,48 @@ class OrderList extends React.Component {
                     if(item.companyId === '1') {
                         this.setState({
                             data_1: item,
-                            editorState_1: editorState
+                            editorState_1: editorState,
+                            fileList_1: photoList
+                        });
+                    } else if(item.companyId === '2') {
+                        this.setState({
+                            data_2: item,
+                            editorState_2: editorState,
+                            fileList_2: photoList
                         });
                     }
+                });
+            }
+        });
+    }
+
+    //获取公司服务信息
+    getServiceList = () => {
+        ajax.getJSON(getServiceListUrl, null, (data) => {
+            if (data.success) {
+                let backData = data.backData;
+                let service_1 = [], service_2 = [];
+                let holiday_1 = [], holiday_2 = [];
+                backData.map(item => {
+                    if(item.companyId === '1'){
+                        if(service_type === '服务咨询'){
+                            service_1.push(item);
+                        }else if(service_type === '节日活动'){
+                            holiday_1.push(item);
+                        }
+                    }else if(item.companyId === '2'){
+                        if(service_type === '服务咨询'){
+                            service_2.push(item);
+                        }else if(service_type === '节日活动'){
+                            holiday_2.push(item);
+                        }
+                    }
+                });
+                this.setState({
+                    service_1,
+                    service_2,
+                    holiday_1,
+                    holiday_2
                 });
             }
         });
@@ -74,12 +181,44 @@ class OrderList extends React.Component {
         });
     }
 
+    updateComPhoto = (companyId) => {
+        const { fileList_1, fileList_2 } = this.state;
+        let param = {};
+        param.companyId = companyId;
+        if(companyId === '1'){
+            param.photo = fileList_1.map((item, index) => {
+                return item.response.data.id;
+            }).join(',');
+        }else if(companyId === '2'){
+            param.photo = fileList_2.map((item, index) => {
+                return item.response.data.id;
+            }).join(',');
+        }
+        
+        ajax.postJSON(updateComPhotoUrl, JSON.stringify(param), (data) => {
+            if (data.success) {
+                alert('保存成功~');
+            }
+        });
+    }
+
 
     detailrouter = (id) => {
         return `/frame/order/orderDetailInfo/${id}`
     }
 
-    handleChange = ({fileList}) => this.setState({fileList})
+    handleChange = (fileList, companyId) => {
+        console.log('fileList == ', fileList);
+        if(companyId === '1'){
+            this.setState({
+                fileList_1: fileList.fileList
+            });
+        }else if(companyId === '2'){
+            this.setState({
+                fileList_2: fileList.fileList
+            });
+        }
+    }
 
     saveEditorState = (editorState, companyId) => {
         if (companyId === '1') {
@@ -99,6 +238,12 @@ class OrderList extends React.Component {
         let {
             editorState_1,
             editorState_2,
+            fileList_1,
+            fileList_2,
+            service_1,
+            service_2,
+            holiday_1,
+            holiday_2,
             loading,
             submitLoading_1,
             submitLoading_2,
@@ -131,13 +276,19 @@ class OrderList extends React.Component {
                                         </Row>
                                     </TabPane>
                                     <TabPane tab="服务咨询" key="1_2">
+                                        <Table 
+                                          bordered={true} 
+                                          dataSource={service_1} 
+                                          columns={this.columns}
+                                        />
                                     </TabPane>
                                     <TabPane tab="企业相册" key="1_3">
                                         <Upload
                                             action={restUrl.UPLOAD}
                                             listType={'picture'}
+                                            fileList={fileList_1}
                                             className='upload-list-inline'
-                                            onChange={this.handleChange}
+                                            onChange={(fileList) => this.handleChange(fileList, '1')}
                                         >
                                             <Button>
                                                 <Icon type="upload"/> 上传
@@ -146,15 +297,65 @@ class OrderList extends React.Component {
                                         <Divider/>
                                         <Row>
                                             <Col>
-                                                <Button type="primary">保存</Button>
+                                                <Button type="primary" onClick={this.updateComPhoto.bind(null, '1')}>保存</Button>
                                             </Col>
                                         </Row>
                                     </TabPane>
                                     <TabPane tab="节日活动" key="1_4">
+                                        <Table 
+                                          bordered={true} 
+                                          dataSource={holiday_1} 
+                                          columns={this.columns}
+                                        />
                                     </TabPane>
                                 </Tabs>
                             </TabPane>
                             <TabPane tab="二楼食堂官网" key="2">
+                                <Tabs defaultActiveKey="1_1">
+                                    <TabPane tab="企业文化" key="1_1">
+                                        <ZZEditor editorState={editorState_2} companyId={'2'}
+                                                  saveEditorState={this.saveEditorState}/>
+                                        <Divider/>
+                                        <Row>
+                                            <Col>
+                                                <Button type="primary" onClick={this.updateComCulture.bind(null, '2')}>保存</Button>
+                                            </Col>
+                                        </Row>
+                                    </TabPane>
+                                    <TabPane tab="服务咨询" key="1_2">
+                                        <Table 
+                                          bordered={true} 
+                                          dataSource={service_2} 
+                                          columns={this.columns}
+                                        />
+                                    </TabPane>
+                                    <TabPane tab="企业相册" key="1_3">
+                                        <Upload
+                                            action={restUrl.UPLOAD}
+                                            listType={'picture'}
+                                            fileList={fileList_2}
+                                            className='upload-list-inline'
+                                            onChange={(fileList) => this.handleChange(fileList, '2')}
+                                        >
+                                            <Button>
+                                                <Icon type="upload"/> 上传
+                                            </Button>
+                                        </Upload>
+                                        <Divider/>
+                                        <Row>
+                                            <Col>
+                                                <Button type="primary" onClick={this.updateComPhoto.bind(null, '2')}>保存</Button>
+                                            </Col>
+                                        </Row>
+                                    </TabPane>
+                                    <TabPane tab="节日活动" key="1_4">
+                                        <Table 
+                                          bordered={true} 
+                                          dataSource={holiday_2} 
+                                          columns={this.columns}
+                                        />
+                                    </TabPane>
+                                </Tabs>
                             </TabPane>
                         </Tabs>
                     </Spin>
